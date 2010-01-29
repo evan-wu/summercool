52d50a084e7ba0afb30790b3661088f2453ae93a
wuhao.arctany <wuhao.arctany@586b8c8a-b5a3-11de-97ce-e783fd196cab>
2010-01-29 06:42:53 +0000

    常量提交

---
+++ hadoop-demo/selleradmin-log-service/src/main/java/com/taobao/selleradminlog/base/constants/HadoopConstants.java
@@ -0,0 +1,16 @@
+package log.test.constancts;
+
+/**
+ * Hadoop 的一般配置参数
+ * 对应hadoop 0.20.1版本
+ * @author wuhao
+ * 
+ *
+ */
+public interface HadoopConstants {
+	
+	public final static String  MAPRED_INPUT_DIR="mapred_input_dir"; 
+	
+	public final static String  MAPRED_OUTPUT_DIR="mapred_output_dir";
+
+}
